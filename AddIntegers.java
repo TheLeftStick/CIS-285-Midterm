3f1d866dc7b7f54f399baf40c989f024cf5ecce4
Balan <balanc@umich.edu>
2021-10-28 16:54:18 -0400

    Int values changed AGAIN

--- AddIntegers.java
+++ AddIntegers.java
@@ -2,8 +2,8 @@ public class AddIntegers {
 
     public static void main(String[] args) {
 
-        int first = 101;
-        int second = 50;
+        int first = 20;
+        int second = 60;
 
         System.out.println("Enter two numbers: " + first + " " + second);
         int sum = first + second;
